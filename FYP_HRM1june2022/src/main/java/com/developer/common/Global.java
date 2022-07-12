7e53c4b552f56df4d0011183a517c30f0948dd8b
matthew <20011105@myrp.edu.sg>
2022-07-12 23:48:21 +0800

    first commit

---
+++ FYP_HRM1june2022/src/main/java/com/developer/common/Global.java
@@ -0,0 +1,21 @@
+package com.developer.common;
+
+public class Global {
+
+	
+//	public static String serverName="localhost";
+//	public static String databaseName="hrms_database";
+//	public static String userName="root";
+//	public static String userPwd="1234";
+
+	public static String serverName="database-1.cjemoshbfjmx.us-west-2.rds.amazonaws.com";
+	public static String databaseName="HRMS_database";
+	public static String userName="admin";
+	public static String userPwd="Matthew31!";
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
