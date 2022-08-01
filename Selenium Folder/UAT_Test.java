d42fc59bdf52c836a7128cc6ca2b32c123b8ed40
maltiee <87346635+maltiee@users.noreply.github.com>
2022-08-02 06:14:21 +0800

    Update UAT_Test.java

--- Selenium Folder/UAT_Test.java
+++ Selenium Folder/UAT_Test.java
@@ -1,6 +1,7 @@
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import io.github.bonigarcia.wdm.WebDriverManager;
 
 public class UAT_Test {
 	public static void main(String[] args) throws InterruptedException {
