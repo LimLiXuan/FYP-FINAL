9b1fa0c8f28ec95ba6efca77f26972a1d409e4ae
maltiee <87346635+maltiee@users.noreply.github.com>
2022-08-02 06:20:10 +0800

    Update ViewPageTesting.java

--- Selenium Folder/ViewPageTesting.java
+++ Selenium Folder/ViewPageTesting.java
@@ -1,7 +1,7 @@
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
-
+import io.github.bonigarcia.wdm.WebDriverManager;
 
 public class ViewPageTesting {
 	public static void main(String[] args) throws InterruptedException {
